0010673dec062f037f8e763fa29a8a03d8dde61e
unknown <osmanmutanuka@gmail.com>
2023-10-03 10:43:54 +0200

    changes-to-branch

---
+++ app/Home.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+export default Home; 
